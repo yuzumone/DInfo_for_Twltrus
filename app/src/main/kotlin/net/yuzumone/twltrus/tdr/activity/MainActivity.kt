213f84605c7178ebecb9dd46945194d27f2f16d7
yuzumone <yuzumone@outlook.com>
2017-06-28 13:37:18 +0900

    Update main activity

---
+++ app/src/main/kotlin/net/yuzumone/twltrus/tdr/activity/MainActivity.kt
@@ -0,0 +1,17 @@
+package net.yuzumone.twltrus.tdr.activity
+
+import android.databinding.DataBindingUtil
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import net.yuzumone.twltrus.tdr.R
+import net.yuzumone.twltrus.tdr.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit private var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+    }
+}
