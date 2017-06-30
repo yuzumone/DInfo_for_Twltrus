25d650c3b88f6c005ef8ad5674a18cda89d1722d
yuzumone <yuzumone@outlook.com>
2017-06-30 21:32:13 +0900

    Change to no action bar theme

--- app/src/main/kotlin/net/yuzumone/twltrus/tdr/activity/MainActivity.kt
+++ app/src/main/kotlin/net/yuzumone/twltrus/tdr/activity/MainActivity.kt
@@ -13,5 +13,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setSupportActionBar(binding.toolbar)
     }
 }
