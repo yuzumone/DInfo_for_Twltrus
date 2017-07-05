ccfc040d4ba1b72b1a728220ce426e3e0488862d
yuzumone <yuzumone@outlook.com>
2017-07-05 17:26:37 +0900

    Add pager adapter

---
+++ app/src/main/kotlin/net/yuzumone/twltrus/tdr/adapter/PagerAdapter.kt
@@ -0,0 +1,29 @@
+package net.yuzumone.twltrus.tdr.adapter
+
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
+
+class PagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+
+    private val fragments = ArrayList<Fragment>()
+    private val titles = ArrayList<String>()
+
+    override fun getItem(position: Int): Fragment {
+        return fragments[position]
+    }
+
+    override fun getCount(): Int {
+        return fragments.size
+    }
+
+    override fun getPageTitle(position: Int): CharSequence {
+        return titles[position]
+    }
+
+    fun add(title: String, fragment: Fragment) {
+        fragments.add(fragment)
+        titles.add(title)
+        notifyDataSetChanged()
+    }
+}
