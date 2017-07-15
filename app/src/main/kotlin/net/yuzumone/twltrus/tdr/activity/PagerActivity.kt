65eb98e5dbb9606d2725c860125ab0ca83b46ebf
yuzumone <yuzumone@outlook.com>
2017-07-15 23:10:24 +0900

    Add pager activity

---
+++ app/src/main/kotlin/net/yuzumone/twltrus/tdr/activity/PagerActivity.kt
@@ -0,0 +1,126 @@
+package net.yuzumone.twltrus.tdr.activity
+
+import android.content.Context
+import android.content.Intent
+import android.databinding.DataBindingUtil
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.MenuItem
+import android.view.View
+import kotlinx.coroutines.experimental.CommonPool
+import kotlinx.coroutines.experimental.android.UI
+import kotlinx.coroutines.experimental.async
+import kotlinx.coroutines.experimental.launch
+import net.yuzumone.twltrus.tdr.R
+import net.yuzumone.twltrus.tdr.adapter.PagerAdapter
+import net.yuzumone.twltrus.tdr.api.AttractionApi
+import net.yuzumone.twltrus.tdr.api.GreetingApi
+import net.yuzumone.twltrus.tdr.api.RestaurantApi
+import net.yuzumone.twltrus.tdr.api.ShowApi
+import net.yuzumone.twltrus.tdr.databinding.ActivityPagerBinding
+import net.yuzumone.twltrus.tdr.extension.convertArrayList
+import net.yuzumone.twltrus.tdr.fragment.AttractionFragment
+import net.yuzumone.twltrus.tdr.fragment.GreetingFragment
+import net.yuzumone.twltrus.tdr.fragment.RestaurantFragment
+import net.yuzumone.twltrus.tdr.fragment.ShowFragment
+
+class PagerActivity : AppCompatActivity() {
+
+    lateinit private var binding: ActivityPagerBinding
+    private val park: Park by lazy {
+        intent.extras.getSerializable(ARG_PARK) as Park
+    }
+    private val adapter: PagerAdapter by lazy {
+        PagerAdapter(supportFragmentManager)
+    }
+
+    companion object {
+        private val ARG_PARK = "park"
+        fun createIntent(context: Context, park: Park) {
+            val intent = Intent(context, PagerActivity::class.java)
+            intent.putExtra(ARG_PARK, park)
+            context.startActivity(intent)
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_pager)
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        binding.pager.adapter = adapter
+        binding.tab.setupWithViewPager(binding.pager)
+        if (park == Park.TDR) {
+            setTitle(R.string.tdl)
+            getTDL()
+        } else if (park == Park.TDS) {
+            setTitle(R.string.tds)
+            getTDS()
+        }
+    }
+
+    private fun getTDL() = launch(UI) {
+        binding.progress.visibility = View.VISIBLE
+        try {
+            val showJob = async(CommonPool) { ShowApi.getTdl() }
+            val attractionJob = async(CommonPool) { AttractionApi.getTdl() }
+            val restaurantJob = async(CommonPool) { RestaurantApi.getTdl() }
+            val greetingJob = async(CommonPool) { GreetingApi.getTdl() }
+            val shows = showJob.await()
+            val attractions = attractionJob.await()
+            val restaurants = restaurantJob.await()
+            val greetings = greetingJob.await()
+            val showFragment = ShowFragment.newInstance(shows.convertArrayList())
+            val attractionFragment = AttractionFragment.newInstance(attractions.convertArrayList())
+            val restaurantFragment = RestaurantFragment.newInstance(restaurants.convertArrayList())
+            val greetingFragment = GreetingFragment.newInstance(greetings.convertArrayList())
+            adapter.add(getString(R.string.show), showFragment)
+            adapter.add(getString(R.string.attraction), attractionFragment)
+            adapter.add(getString(R.string.restaurant), restaurantFragment)
+            adapter.add(getString(R.string.greeting), greetingFragment)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        binding.progress.visibility = View.GONE
+    }
+
+    private fun getTDS() = launch(UI) {
+        binding.progress.visibility = View.VISIBLE
+        try {
+            val showJob = async(CommonPool) { ShowApi.getTds() }
+            val attractionJob = async(CommonPool) { AttractionApi.getTds() }
+            val restaurantJob = async(CommonPool) { RestaurantApi.getTds() }
+            val greetingJob = async(CommonPool) { GreetingApi.getTds() }
+            val shows = showJob.await()
+            val attractions = attractionJob.await()
+            val restaurants = restaurantJob.await()
+            val greetings = greetingJob.await()
+            val showFragment = ShowFragment.newInstance(shows.convertArrayList())
+            val attractionFragment = AttractionFragment.newInstance(attractions.convertArrayList())
+            val restaurantFragment = RestaurantFragment.newInstance(restaurants.convertArrayList())
+            val greetingFragment = GreetingFragment.newInstance(greetings.convertArrayList())
+            adapter.add(getString(R.string.show), showFragment)
+            adapter.add(getString(R.string.attraction), attractionFragment)
+            adapter.add(getString(R.string.restaurant), restaurantFragment)
+            adapter.add(getString(R.string.greeting), greetingFragment)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        binding.progress.visibility = View.GONE
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            android.R.id.home -> {
+                finish()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    enum class Park {
+        TDR,
+        TDS
+    }
+}
