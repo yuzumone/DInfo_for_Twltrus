aa2b5a84ee083818d84fd4f02dd7edf9ccab0006
yuzumone <yuzumone@outlook.com>
2017-09-27 10:08:28 +0900

    Remove paper parcel

--- app/src/main/kotlin/net/yuzumone/twltrus/tdr/model/Restaurant.kt
+++ app/src/main/kotlin/net/yuzumone/twltrus/tdr/model/Restaurant.kt
@@ -1,17 +1,45 @@
 package net.yuzumone.twltrus.tdr.model
 
-import paperparcel.PaperParcel
-import paperparcel.PaperParcelable
+import android.os.Parcel
+import android.os.Parcelable
 
-@PaperParcel
 data class Restaurant(
         val name: String,
         val wait: String,
         val run: String,
         val left: String,
         val right: String,
-        val url: String) : PaperParcelable {
-    companion object {
-        @JvmField val CREATOR = PaperParcelRestaurant.CREATOR
+        val url: String) : Parcelable {
+
+    constructor(parcel: Parcel) : this(
+            parcel.readString(),
+            parcel.readString(),
+            parcel.readString(),
+            parcel.readString(),
+            parcel.readString(),
+            parcel.readString()) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(name)
+        parcel.writeString(wait)
+        parcel.writeString(run)
+        parcel.writeString(left)
+        parcel.writeString(right)
+        parcel.writeString(url)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Restaurant> {
+        override fun createFromParcel(parcel: Parcel): Restaurant {
+            return Restaurant(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Restaurant?> {
+            return arrayOfNulls(size)
+        }
     }
 }
--- app/src/main/kotlin/net/yuzumone/twltrus/tdr/model/Show.kt
+++ app/src/main/kotlin/net/yuzumone/twltrus/tdr/model/Show.kt
@@ -1,14 +1,36 @@
 package net.yuzumone.twltrus.tdr.model
 
-import paperparcel.PaperParcel
-import paperparcel.PaperParcelable
+import android.os.Parcel
+import android.os.Parcelable
 
-@PaperParcel
 data class Show(
         val name: String,
         val time: String,
-        val url: String) : PaperParcelable {
-    companion object {
-        @JvmField val CREATOR = PaperParcelShow.CREATOR
+        val url: String) : Parcelable {
+
+    constructor(parcel: Parcel) : this(
+            parcel.readString(),
+            parcel.readString(),
+            parcel.readString()) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(name)
+        parcel.writeString(time)
+        parcel.writeString(url)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Show> {
+        override fun createFromParcel(parcel: Parcel): Show {
+            return Show(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Show?> {
+            return arrayOfNulls(size)
+        }
     }
 }
