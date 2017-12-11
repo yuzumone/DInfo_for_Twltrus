8acc5fb86092b1a9525e5e1eda28459f65df86bd
yuzumone <yuzumone@outlook.com>
2017-12-11 15:32:38 +0900

    Display park status
    close #20

--- app/src/main/kotlin/net/yuzumone/twltrus/tdr/api/StatusApi.kt
+++ app/src/main/kotlin/net/yuzumone/twltrus/tdr/api/StatusApi.kt
@@ -7,13 +7,21 @@ object StatusApi {
     private val tdlUrl = "http://info.tokyodisneyresort.jp/s/today/sp_tdl_ticket.html"
     private val tdsUrl = "http://info.tokyodisneyresort.jp/s/today/sp_tds_ticket.html"
 
-    fun isCloseTDL(): Boolean {
+    fun getTdlStatus(): String {
+        return Jsoup.connect(tdlUrl).get().select("p").text()
+    }
+
+    fun getTdsStatus(): String {
+        return Jsoup.connect(tdsUrl).get().select("p").text()
+    }
+
+    fun isCloseTdl(): Boolean {
         val doc = Jsoup.connect(tdlUrl).get()
         val item = doc.select("p")
         return item.text() == "ただいま東京ディズニーランドは、閉園しております。"
     }
 
-    fun isCloseTDS(): Boolean {
+    fun isCloseTds(): Boolean {
         val doc = Jsoup.connect(tdsUrl).get()
         val item = doc.select("p")
         return item.text() == "ただいま東京ディズニーシーは、閉園しております。"
