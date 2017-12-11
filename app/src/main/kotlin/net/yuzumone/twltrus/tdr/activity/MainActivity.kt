e2e7cd52b9f408d975a82cb4241bcd603b25c99f
yuzumone <yuzumone@outlook.com>
2017-12-11 15:32:57 +0900

    Display park status

--- app/src/main/kotlin/net/yuzumone/twltrus/tdr/activity/MainActivity.kt
+++ app/src/main/kotlin/net/yuzumone/twltrus/tdr/activity/MainActivity.kt
@@ -3,7 +3,11 @@ package net.yuzumone.twltrus.tdr.activity
 import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.coroutines.experimental.android.UI
+import kotlinx.coroutines.experimental.async
+import kotlinx.coroutines.experimental.launch
 import net.yuzumone.twltrus.tdr.R
+import net.yuzumone.twltrus.tdr.api.StatusApi
 import net.yuzumone.twltrus.tdr.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -14,11 +18,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         setSupportActionBar(binding.toolbar)
+        getStatus()
         binding.buttonTdl.setOnClickListener {
             PagerActivity.createIntent(this, PagerActivity.Park.TDR)
         }
         binding.buttonTds.setOnClickListener {
             PagerActivity.createIntent(this, PagerActivity.Park.TDS)
         }
+    }
+
+    private fun getStatus() = launch(UI) {
+        val tdlJob = async { StatusApi.getTdlStatus() }
+        val tdsJob = async { StatusApi.getTdsStatus() }
+        val tdlStatus = tdlJob.await()
+        val tdsStatus = tdsJob.await()
+        binding.textTdlStatus.text = tdlStatus
+        binding.textTdsStatus.text = tdsStatus
     }
 }
