6a1f4eb96c56d3a0a6c21db8bd9c2c1fc95fcfa9
yuzumone <yuzumone@outlook.com>
2018-01-31 10:14:44 +0900

    Fix no data error
    close #32

--- app/src/main/kotlin/net/yuzumone/twltrus/tdr/api/RehabApi.kt
+++ app/src/main/kotlin/net/yuzumone/twltrus/tdr/api/RehabApi.kt
@@ -1,5 +1,6 @@
 package net.yuzumone.twltrus.tdr.api
 
+import net.yuzumone.twltrus.tdr.extension.regularHeader
 import net.yuzumone.twltrus.tdr.model.Rehab
 import org.jsoup.Jsoup
 import org.jsoup.select.Elements
@@ -9,13 +10,13 @@ object RehabApi {
     private val url = "http://info.tokyodisneyresort.jp/schedule/stop/stop_list.html"
 
     fun getTdl(): List<Rehab> {
-        val doc = Jsoup.connect(url).followRedirects(true).get()
+        val doc = Jsoup.connect(url).followRedirects(true).regularHeader().get()
         val list = doc.select("section.tdl > dl > dd > ul> li")
         return analysis(list)
     }
 
     fun getTds(): List<Rehab> {
-        val doc = Jsoup.connect(url).followRedirects(true).get()
+        val doc = Jsoup.connect(url).followRedirects(true).regularHeader().get()
         val list = doc.select("section.tds > dl > dd > ul> li")
         return analysis(list)
     }
---
+++ app/src/main/kotlin/net/yuzumone/twltrus/tdr/extension/JsoupExtension.kt
@@ -0,0 +1,18 @@
+package net.yuzumone.twltrus.tdr.extension
+
+import org.jsoup.Connection
+
+private val agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) " +
+        "Chrome/63.0.3239.132 Safari/537.36"
+private val accept = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8"
+private val encoding = "gzip, deflate"
+private val language = "ja-JP,ja;q=0.9,en-US;q=0.8,en;q=0.7"
+
+fun Connection.regularHeader(): Connection {
+    return this.userAgent(agent)
+            .header("Accept", accept)
+            .header("Accept-Encoding", encoding)
+            .header("Accept-Language", language)
+            .header("Upgrade-Insecure-Requests", "1")
+            .header("Connection", "keep-alive")
+}
