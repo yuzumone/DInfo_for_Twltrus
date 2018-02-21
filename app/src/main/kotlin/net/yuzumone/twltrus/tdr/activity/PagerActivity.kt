965ead43dfc12db0fe4c7c79aeb271cbb421daca
yuzumone <yuzumone@outlook.com>
2018-02-21 12:52:13 +0900

    Add refresh
    close #41

--- app/src/main/kotlin/net/yuzumone/twltrus/tdr/activity/PagerActivity.kt
+++ app/src/main/kotlin/net/yuzumone/twltrus/tdr/activity/PagerActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import android.widget.Toast
@@ -22,7 +23,7 @@ import net.yuzumone.twltrus.tdr.model.Park
 
 class PagerActivity : AppCompatActivity() {
 
-    lateinit private var binding: ActivityPagerBinding
+    private lateinit var binding: ActivityPagerBinding
     private val park: Park by lazy {
         intent.extras.getSerializable(ARG_PARK) as Park
     }
@@ -31,7 +32,7 @@ class PagerActivity : AppCompatActivity() {
     }
 
     companion object {
-        private val ARG_PARK = "park"
+        private const val ARG_PARK = "park"
         fun createIntent(context: Context, park: Park) {
             val intent = Intent(context, PagerActivity::class.java)
             intent.putExtra(ARG_PARK, park)
@@ -46,6 +47,11 @@ class PagerActivity : AppCompatActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         binding.pager.adapter = adapter
         binding.tab.setupWithViewPager(binding.pager)
+        refresh()
+    }
+
+    private fun refresh() {
+        adapter.clear()
         if (park == Park.TDL) {
             setTitle(R.string.tdl)
             getTDL()
@@ -131,10 +137,19 @@ class PagerActivity : AppCompatActivity() {
         binding.progress.visibility = View.GONE
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_pager, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
         when (item?.itemId) {
             android.R.id.home -> {
                 finish()
+                return true
+            }
+            R.id.menu_refresh -> {
+                refresh()
                 return true
             }
         }
