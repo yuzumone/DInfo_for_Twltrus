7c4686184a6a604a63a200561b14c4dfbbc0c234
yuzumone <yuzumone@outlook.com>
2018-04-21 13:54:18 +0900

    Add cookie api
    close #46

--- app/src/main/kotlin/net/yuzumone/twltrus/tdr/extension/JsoupExtension.kt
+++ app/src/main/kotlin/net/yuzumone/twltrus/tdr/extension/JsoupExtension.kt
@@ -5,8 +5,8 @@ import org.jsoup.Connection
 private const val agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) " +
         "Chrome/63.0.3239.132 Safari/537.36"
 private const val accept = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8"
-private const val encoding = "gzip, deflate"
-private const val language = "ja-JP,ja;q=0.9,en-US;q=0.8,en;q=0.7"
+private const val encoding = "gzip, deflate, br"
+private const val language = "ja"
 
 fun Connection.regularHeader(): Connection {
     return this.userAgent(agent)
@@ -15,4 +15,14 @@ fun Connection.regularHeader(): Connection {
             .header("Accept-Language", language)
             .header("Upgrade-Insecure-Requests", "1")
             .header("Connection", "keep-alive")
+}
+
+fun Connection.cookieHeader(): Connection {
+    return this.userAgent(agent)
+            .header("Accept", "application/json, text/javascript, */*; q=0.01")
+            .header("Accept-Encoding", encoding)
+            .header("Accept-Language", language)
+            .header("Connection", "keep-alive")
+            .header("X-Requested-With", "XMLHttpRequest")
+            .header("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8")
 }
