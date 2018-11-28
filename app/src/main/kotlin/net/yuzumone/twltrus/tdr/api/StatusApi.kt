333a5f4a49d2f171a36c8a8d821e8302c7eb9f44
yuzumone <yuzumone@outlook.com>
2018-11-28 19:47:46 +0900

    Fix issue that cannot get status

--- app/src/main/kotlin/net/yuzumone/twltrus/tdr/api/StatusApi.kt
+++ app/src/main/kotlin/net/yuzumone/twltrus/tdr/api/StatusApi.kt
@@ -5,25 +5,27 @@ import org.jsoup.Jsoup
 
 object StatusApi {
 
-    private val tdlUrl = "http://info.tokyodisneyresort.jp/s/today/sp_tdl_ticket.html"
-    private val tdsUrl = "http://info.tokyodisneyresort.jp/s/today/sp_tds_ticket.html"
+    private const val tdlUrl =
+            "https://www.tokyodisneyresort.jp/view_interface.php?blockId=94199&pageBlockId=135360"
+    private const val tdsUrl =
+            "https://www.tokyodisneyresort.jp/view_interface.php?blockId=94199&pageBlockId=135410"
 
     fun getTdlStatus(): String {
-        return Jsoup.connect(tdlUrl).regularHeader().get().select("p").text()
+        return Jsoup.connect(tdlUrl).regularHeader().ignoreContentType(true).get().select("p").text()
     }
 
     fun getTdsStatus(): String {
-        return Jsoup.connect(tdsUrl).regularHeader().get().select("p").text()
+        return Jsoup.connect(tdsUrl).regularHeader().ignoreContentType(true).get().select("p").text()
     }
 
     fun isCloseTdl(): Boolean {
-        val doc = Jsoup.connect(tdlUrl).regularHeader().get()
+        val doc = Jsoup.connect(tdlUrl).regularHeader().ignoreContentType(true).get()
         val item = doc.select("p")
         return item.text() == "ただいま東京ディズニーランドは、閉園しております。"
     }
 
     fun isCloseTds(): Boolean {
-        val doc = Jsoup.connect(tdsUrl).regularHeader().get()
+        val doc = Jsoup.connect(tdsUrl).regularHeader().ignoreContentType(true).get()
         val item = doc.select("p")
         return item.text() == "ただいま東京ディズニーシーは、閉園しております。"
     }
