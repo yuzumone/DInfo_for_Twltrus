6101504f53eb28cdbe868b72c22e76bfc9e3cecd
yuzumone <yuzumone@outlook.com>
2018-12-03 20:36:39 +0900

    Update MainActivity

--- app/src/main/kotlin/net/yuzumone/twltrus/tdr/ui/MainActivity.kt
+++ app/src/main/kotlin/net/yuzumone/twltrus/tdr/ui/MainActivity.kt
@@ -2,55 +2,52 @@ package net.yuzumone.twltrus.tdr.ui
 
 import androidx.databinding.DataBindingUtil
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import android.view.View
 import kotlinx.coroutines.experimental.android.UI
 import kotlinx.coroutines.experimental.async
 import kotlinx.coroutines.experimental.launch
 import net.yuzumone.twltrus.tdr.R
+import net.yuzumone.twltrus.tdr.R.string.refresh
 import net.yuzumone.twltrus.tdr.data.api.CookieApi
 import net.yuzumone.twltrus.tdr.data.api.StatusApi
 import net.yuzumone.twltrus.tdr.databinding.ActivityMainBinding
 import net.yuzumone.twltrus.tdr.model.Park
+import net.yuzumone.twltrus.tdr.ui.top.TopFragment
 import net.yuzumone.twltrus.tdr.utils.PrefUtil
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private val pref by lazy { PrefUtil(this) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         setSupportActionBar(binding.toolbar)
-        getStatus()
-        binding.buttonTdl.setOnClickListener {
-            PagerActivity.createIntent(this, Park.TDL)
+        supportFragmentManager.addOnBackStackChangedListener {
+            if (supportFragmentManager.backStackEntryCount > 0) {
+                supportActionBar?.setDisplayHomeAsUpEnabled(true)
+                binding.toolbar.elevation = 0F
+            } else {
+                supportActionBar?.setDisplayHomeAsUpEnabled(false)
+                binding.toolbar.elevation = 8F
+                setTitle(R.string.app_name)
+            }
         }
-        binding.buttonTds.setOnClickListener {
-            PagerActivity.createIntent(this, Park.TDS)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction().add(R.id.content, TopFragment()).commit()
         }
     }
 
-    private fun getStatus() = launch(UI) {
-        try {
-            binding.progress.visibility = View.VISIBLE
-            if (pref.shouldGetCookie) {
-                val cookieJob = async { CookieApi.getCookie() }
-                val cookie = cookieJob.await()
-                pref.date = Date()
-                pref.cookie = cookie
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            android.R.id.home -> {
+                onBackPressed()
+                return true
             }
-            val tdlJob = async { StatusApi.getTdlStatus() }
-            val tdsJob = async { StatusApi.getTdsStatus() }
-            val tdlStatus = tdlJob.await()
-            val tdsStatus = tdsJob.await()
-            binding.progress.visibility = View.GONE
-            binding.textTdlStatus.text = tdlStatus
-            binding.textTdsStatus.text = tdsStatus
-        } catch (e: Exception) {
-            e.printStackTrace()
         }
+        return super.onOptionsItemSelected(item)
     }
 }
